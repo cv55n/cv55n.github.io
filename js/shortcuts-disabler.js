432595a30ef4e0f89b57988d5b901373706d043e
cavassani <lucas.cavassani.q@gmail.com>
2025-05-31 17:04:19 -0300

    implementação de algumas novas medidas de segurança

--- js/shortcuts-disabler.js
+++ js/shortcuts-disabler.js
@@ -4,6 +4,11 @@ document.addEventListener('keydown', function(e) {
         e.preventDefault();
     }
 
+    // ctrl + s
+    if (e.ctrlKey && e.key === 's') {
+        e.preventDefault();
+    }
+
     // ctrl + shift + i
     if (e.ctrlKey && e.shiftKey && e.keyCode === 73) {
         e.preventDefault();
